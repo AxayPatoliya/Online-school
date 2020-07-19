02affca3708c989d59816e09af5985876feffb70
dailyblogs2021 <dailyblogs,team@gmail.com>
2020-07-19 21:30:46 +0530

    make it better

--- notice_seeds.sql
+++ notice_seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO notice(
 	sno, category, title, content, img_file, date, link, slug)
-	VALUES (1, 'category of notice', 'title of notice', 'content of notice', '20200608_110050_0000.png', 'https://dailyblogs2020.herokuapp.com/', '2020-07-15', 'first-notice');
+	VALUES (1, 'category of notice', 'title of notice', 'content of notice', '20200608_110050_0000.png', '2020-07-19', 'https://dailyblogs2020.herokuapp.com/', 'first-notice');
